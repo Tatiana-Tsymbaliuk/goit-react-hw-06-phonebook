b31154d24311d36fcfdda038998ed79ca7dd4743
Tatiana-Tsymbaliuk <114348739+Tatiana-Tsymbaliuk@users.noreply.github.com>
2023-06-30 20:58:54 +0300

    add clean

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,43 +7,6 @@ import Filter from 'components/Filter/Filter';
 
 
 export default function App(){
-  //const parsedContacts = JSON.parse(localStorage.getItem("contacts"));
-  // const [contacts, setContacts] = useState(parsedContacts)
-  // const {contacts} = useSelector(state => state.phonebook);
-  // const dispatch = useDispatch();
-
-// const handleSubmit = data => {
-//   const newContacts ={
-//     id: nanoid(), 
-//     ...data 
-//   }
-  
-//     const someContact = contacts.some(item=> item.name.toLowerCase() === data.name.toLowerCase())
-//       if (!someContact) {
-//         dispatch(addContact(newContacts));      
-//       } else{
-//       alert(`Name is already in contacts`);  }   
-//       }
-
-    // const changeFilter =(e)=>{
-    //   setFilter(e.currentTarget.value)
-    // } 
-
-    // const deleteTodo = (todoId) =>{
-    //   setContacts(prevState =>(
-    //   prevState.filter(contact=>contact.id !== todoId)
-    // ));}
-
-  //   useEffect(() => {
-  //   localStorage.setItem("contacts", JSON.stringify(contacts));
-  // }, [contacts]);
-      
-    //     const getVisibleContacts=()=>{   
-    //     const normalizedFilter = filter.toLowerCase();
-    //     return contacts.filter((contact) => contact.name.toLowerCase().includes(normalizedFilter));
-    //     }
-   
-    // const visibleContacts = getVisibleContacts()
     return (<div>
       <h1>Phonebook</h1>
       <ContactForm />
